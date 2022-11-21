4bc19cf87385942481a0906a8bf5cb4de37a2be0
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-21 21:23:30 +0530

    [TASK]: encrption of password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -42,5 +42,18 @@ const userSchema = mongoose.Schema(
     }
 );
 
+//   Encrypt password before saving to DB
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+      return next();
+    }
+  
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  });
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
