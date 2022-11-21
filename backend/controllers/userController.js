70050ac4af2d18418ac31a8d5c4392f65a2443d9
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-21 15:14:12 +0530

    [TASK]: error middleware creation for error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,17 @@
-const registerUser = async (req,res) => {
+const asyncHandler = require("express-async-handler");
+
+const registerUser = asyncHandler(async(req,res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      res.status(400);
+      throw new Error("Please fill in all required fields");
+    }
+    if (password.length < 6) {
+      res.status(400);
+      throw new Error("Password must be up to 6 characters");
+    }
     res.send("User registered")
-}
+});
 
 module.exports = {
     registerUser,
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRoute = require("./routers/userRouter");
+const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
 
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(errorHandler);
+
 
 //Routers
 
