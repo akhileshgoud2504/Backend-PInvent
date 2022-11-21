05750cc3d7bd329c3ba8371d91a7b276b76a927e
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-21 15:29:56 +0530

    [TASK]: creation of user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,10 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async(req,res) => {
     const { name, email, password } = req.body;
+
+    //validations for mandatory checks
     if (!name || !email || !password) {
       res.status(400);
       throw new Error("Please fill in all required fields");
@@ -10,6 +13,37 @@ const registerUser = asyncHandler(async(req,res) => {
       res.status(400);
       throw new Error("Password must be up to 6 characters");
     }
+
+    //checking whether user already exists
+    const userExists = await User.findOne({email});
+    if(userExists){
+        res.status(400);
+        throw new Error("Email already exists, Please try with another email");
+    }
+
+    //Create a new user
+    const  user = await User.create({
+        name: name,
+        email:email,
+        password:password
+    }) 
+
+    if(user){
+        const { _id, name, email, photo, phone, bio } = user;
+        res.status(201).json({
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio,
+        });
+    } else{
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+
+
     res.send("User registered")
 });
 
