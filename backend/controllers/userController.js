ae087b5ea9929aebaf8dfe7b267df80a33f189ad
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-21 21:43:02 +0530

    [TASK]: json web token creation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const generateToken = (id) =>{
+    return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:'1d'})
+}
 
 const registerUser = asyncHandler(async(req,res) => {
     const { name, email, password } = req.body;
@@ -28,6 +34,9 @@ const registerUser = asyncHandler(async(req,res) => {
         password:password
     }) 
 
+    //genrating a token
+    const token = generateToken(user._id);
+
     if(user){
         const { _id, name, email, photo, phone, bio } = user;
         res.status(201).json({
@@ -37,6 +46,7 @@ const registerUser = asyncHandler(async(req,res) => {
             photo,
             phone,
             bio,
+            token
         });
     } else{
         res.status(400);
