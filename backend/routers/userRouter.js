a9968f70e7dfd014cc25bb52c031c3628a743d95
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-22 15:37:09 +0530

    [TASK]: big ifx in getuser

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -8,6 +8,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login",loginUser);
 router.get("/logout",logoutUser);
-router.get("getuser",protect,getUser);
+router.get("/getuser",protect,getUser);
 
 module.exports = router;
