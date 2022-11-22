2b9ef45260cf068dc06f1f764a8e7a46e8b22900
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-22 15:31:28 +0530

    [TASK]L: get user with protection middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -127,8 +127,27 @@ const loginUser = asyncHandler(async (req, res) => {
     return res.status(200).json({ message: "Successfully Logged Out" });
   });
 
+  const getUser =  asyncHandler(async (req, res) => {
+        const user = await User.findById(req.user._id);
+        if (user) {
+          const { _id, name, email, photo, phone, bio } = user;
+          res.status(200).json({
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio,
+          });
+        } else {
+          res.status(400);
+          throw new Error("User Not Found");
+        }
+  });
+
 module.exports = {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    getUser
 }
