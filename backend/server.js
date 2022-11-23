bf90070f154bceb7ad440a3a50b5ade59d316096
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-23 23:24:25 +0530

    [TASK]: updating user

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(
+    cors({
+      origin: ["http://localhost:3000", "https://pinvent.vercel.app"],
+      credentials: true,
+    })
+  );
 
 app.use(errorHandler);
 
