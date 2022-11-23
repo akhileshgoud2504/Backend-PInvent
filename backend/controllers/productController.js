912a4d14e2833f208edfd66b7ac0bac34fc442c7
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-23 16:36:05 +0530

    [TASK]: extra validation

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,11 +3,16 @@ const Product = require("../models/productModel");
 
 const createProduct = asyncHandler(async (req, res) => {
     const { name, sku, category, quantity, price, description } = req.body;
-  
+
     //   Validation
     if (!name || !category || !quantity || !price || !description) {
       res.status(400);
       throw new Error("Please fill in all fields");
+    }
+    let productExists = await Product.exists({name: name}); 
+    if(!!productExists){
+        res.status(400);
+        throw new Error("Product already exists");
     }
     const product = await Product.create({
         user: req.user.id,
