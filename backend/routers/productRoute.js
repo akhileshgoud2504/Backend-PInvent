ca73d0bf2a2015fbc1dcc523158bbf962d58ede4
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-23 17:53:32 +0530

    [TASK]: get product

--- backend/routers/productRoute.js
+++ backend/routers/productRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { createProduct, getProducts } = require("../controllers/productController");
+const { createProduct, getProducts, getProduct } = require("../controllers/productController");
 const protect = require("../middleware/authMiddleware");
 const { upload } = require("../utils/fileUpload");
 const router = express.Router();
 
 router.post("/", protect,upload.single("image"), createProduct);
 router.get("/", protect, getProducts);
+router.get("/:id", protect, getProduct);
 
 module.exports = router;
 
