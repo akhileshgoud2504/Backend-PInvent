91e9b19d5baf4547b4d0b96215c90438725444cb
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-23 17:57:07 +0530

    [TASK]: delete product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -71,8 +71,25 @@ const getProduct = asyncHandler(async (req, res) => {
   res.status(200).json(product);
 });
 
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  // if product doesnt exist
+  if (!product) {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+  // Match product to its user
+  if (product.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+  await product.remove();
+  res.status(200).json({ message: "Product deleted." });
+});
+
 module.exports = {
     createProduct,
     getProducts,
-    getProduct
+    getProduct,
+    deleteProduct
 }
